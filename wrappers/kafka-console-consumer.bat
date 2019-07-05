2143845d0b0ed15c604d2c5424ae69bce1e241b9
Marc Kusters <marc.kusters@schiphol.nl>
2019-07-05 17:41:01 +0200

    readme's master branch link, added more wrappers

---
+++ wrappers/kafka-console-consumer.bat
@@ -0,0 +1,12 @@
+@echo off
+
+for /f %%i in ('docker ps ^| findstr cp-kafka-connect') do set container-id=%%i
+echo %container-id%
+
+set kafka-args=%1 %2 %3 %4 %5 %6 %7 %8 %9
+for /L %%i in (0,1,8) do @shift
+set kafka-args=%kafka-args% %1 %2 %3 %4 %5 %6 %7 %8 %9
+for /L %%i in (0,1,8) do @shift
+set kafka-args=%kafka-args% %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+docker exec -i %container-id% kafka-console-consumer %kafka-args%
---
+++ wrappers/ksql.bat
@@ -0,0 +1,12 @@
+@echo off
+
+for /f %%i in ('docker ps ^| findstr cp-ksql-cli') do set container-id=%%i
+echo %container-id%
+
+set ksql-args=%1 %2 %3 %4 %5 %6 %7 %8 %9
+for /L %%i in (0,1,8) do @shift
+set ksql-args=%ksql-args% %1 %2 %3 %4 %5 %6 %7 %8 %9
+for /L %%i in (0,1,8) do @shift
+set ksql-args=%ksql-args% %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+docker exec -it %container-id% /bin/bash ksql %kafka-args%
